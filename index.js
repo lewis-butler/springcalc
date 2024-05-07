068a7081ae5d63382faf9760fe3594606e1fab7e
lewis-butler <157645625+lewis-butler@users.noreply.github.com>
2024-05-07 22:02:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const btnEl = document.getElementById("calculate");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
+const extraInput = document.getElementById("extra");
 const totalSpan = document.getElementById("total");
 
 function calculateTotal() {
   const billValue = billInput.value;
   const tipValue = tipInput.value;
-  const totalValue = billValue * (1 + tipValue / 100);
+  const extraValue = extraInput.value;
+  const totalValue = (billValue * (1 + tipValue / 100)) + extraValue;
   totalSpan.innerText = totalValue.toFixed(2);
 }
 
